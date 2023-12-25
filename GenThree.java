41aa7e5bcff94480d9f977d20f1b99d36df19483
Omri Pinchas <omripinchas.davidov@post.runi.ac.il>
2023-12-25 19:07:31 +0200

    Changed GenThree again.

--- GenThree.java
+++ GenThree.java
@@ -2,9 +2,9 @@
  * Generates three random integers, each in a given range [a,b),
  * prints them, and then prints the minimal number that was generated.
  */
-public class GenThree {
+        public class GenThree {
 	public static void main(String[] args) {
-	        int min, max, difference;
+	int min, max, difference;
         double a, b, c;
         int x, y, z;
         min = Integer.parseInt(args[0]);
